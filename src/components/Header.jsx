28c1df4e1fb05262068ccf1fe8669b33dd31c2f8
Kuznetsov Maxim <m.kuznecov@z-it.ru>
2021-06-16 13:18:29 +0300

    Structure grid

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header className='header'>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/9/9a/VK_Logo.png'/>
+        </header>
+    );
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+function Profile() {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src='https://images.unsplash.com/photo-1623650915002-4a0819173cd9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80'/>
+            </div>
+            <div>
+                ava + desc
+            </div>
+            <div>
+                my posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 2
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Profile
